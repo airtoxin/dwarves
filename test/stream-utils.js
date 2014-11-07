6b5b1af74dde066904b3ccbf0e05db5a9284fe5a
airtoxin <tahirimaka6@gmail.com>
2014-11-07 10:25:39 +0900

    Add toStream function

---
+++ test/stream-utils.js
@@ -0,0 +1,26 @@
+var assert = require( 'assert' ),
+	_      = require( 'lodash' ),
+	spec   = require( 'stream-spec' ),
+	stream = require( '../' );
+
+describe( 'stream-utils', function () {
+	describe( 'toStream', function () {
+		it( 'is readable stream', function ( done ) {
+			var arr = [ 'start', 1, 2, 3, 4, 5, 'end' ];
+			spec( stream.toStream( arr ) )
+				.readable()
+				.pausable( { strict: true } )
+				.validateOnExit();
+			done();
+		} );
+
+		it( 'streams each data of array', function ( done ) {
+			var arr = [ 'start', 1, 2, 3, 4, 5, 'end' ];
+			var s = stream.toStream( arr );
+			_.each( arr, function ( data ) {
+				assert.equal( s.read(), data );
+			} );
+			done();
+		} );
+	} );
+} );
