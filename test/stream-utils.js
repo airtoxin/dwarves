e8ab594a5d70e4e6a1d780fcf047d6a7ac32a9f0
airtoxin <tahirimaka6@gmail.com>
2014-11-07 14:05:34 +0900

    Add reduceStream function

--- test/stream-utils.js
+++ test/stream-utils.js
@@ -31,11 +31,11 @@ describe( 'stream-utils', function () {
 	} );
 
 	describe( 'mapStream', function () {
-		it( 'is duplex stream', function ( done ) {
-			var mapper = function ( data, callback ) {
-				callback();
-			};
-			spec( stream.mapStream( mapper ) )
+		it( 'is transform stream', function ( done ) {
+			var mapper = function ( data, callback ) { callback(); };
+			var m = stream.mapStream( mapper );
+			assert.ok( _.has( m, '_transform' ) );
+			spec( m )
 				.duplex( { strict: true } )
 				.validateOnExit();
 			done();
@@ -63,6 +63,73 @@ describe( 'stream-utils', function () {
 			var m = stream.mapStream( mapper );
 			s.pipe( m ).on( 'finish', function () {
 				assert.deepEqual( processed, arr );
+				done();
+			} );
+		} );
+	} );
+
+	describe( 'reduceStream', function () {
+		it( 'is transform stream', function ( done ) {
+			var reducer = function ( accum, data, callback ) { callback(); };
+			var r = stream.reduceStream( null, reducer );
+			assert.ok( _.has( r, '_transform' ) );
+			spec( r )
+				.duplex( { strict: true } )
+				.validateOnExit();
+			done();
+		} );
+
+		it( 'calls finish event on all data processed', function ( done ) {
+			var arr = _.range(1000);
+			var s = stream.toStream( arr );
+			var reducer = function ( accum, data, callback ) { callback(); };
+			var r = stream.reduceStream( null, reducer );
+			s.pipe( r ).on( 'finish', function () {
+				done();
+			} );
+		} );
+
+		it( 'processes data uning reducer callback', function ( done ) {
+			var processed = [];
+			var reducer = function ( accum, data, callback ) {
+				processed.push( data );
+				callback();
+			};
+			var arr = _.range(1000);
+			var s = stream.toStream( arr );
+
+			var r = stream.reduceStream( null, reducer );
+			s.pipe( r ).on( 'finish', function () {
+				assert.deepEqual( processed, arr );
+				done();
+			} );
+		} );
+
+		it( 'initializer assign to accum first', function ( done ) {
+			var initializer = 'this is initial';
+			var reducer = function ( accum, data, callback ) {
+				assert.equal( accum, initializer );
+				done();
+			};
+			var arr = _.range(1000);
+			var s = stream.toStream( arr );
+
+			var r = stream.reduceStream( initializer, reducer );
+			s.pipe( r );
+		} );
+
+		it( 'accum gets reduced data', function ( done ) {
+			var initializer = 0;
+			var reducer = function ( accum, data, callback ) {
+				accum += data;
+				callback( accum );
+			};
+			var arr = _.range(1000);
+			var s = stream.toStream( arr );
+
+			var r = stream.reduceStream( initializer, reducer );
+			s.pipe( r ).on( 'data', function ( data ) {
+				assert.equal( data, _.reduce( arr, function ( sum, num ) { return sum + num; } ) );
 				done();
 			} );
 		} );
