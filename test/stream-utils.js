1dddefd3d79efff449d1bcaa0b44fca612dec781
airtoxin <tahirimaka6@gmail.com>
2014-11-07 13:40:37 +0900

    Add mapStream function

--- test/stream-utils.js
+++ test/stream-utils.js
@@ -29,4 +29,42 @@ describe( 'stream-utils', function () {
 			s.on( 'data', function(){} ).on( 'end', done );
 		} )
 	} );
+
+	describe( 'mapStream', function () {
+		it( 'is duplex stream', function ( done ) {
+			var mapper = function ( data, callback ) {
+				callback();
+			};
+			spec( stream.mapStream( mapper ) )
+				.duplex( { strict: true } )
+				.validateOnExit();
+			done();
+		} );
+
+		it( 'calls finish event on all data processed', function ( done ) {
+			var arr = _.range(1000);
+			var s = stream.toStream( arr );
+			var mapper = function ( data, callback ) { return callback(); };
+			var m = stream.mapStream( mapper );
+			s.pipe( m ).on( 'finish', function () {
+				done();
+			} );
+		} )
+
+		it( 'processes data uning mapper callback', function ( done ) {
+			var processed = [];
+			var mapper = function ( data, callback ) {
+				processed.push( data );
+				callback();
+			};
+			var arr = _.range(1000);
+			var s = stream.toStream( arr );
+
+			var m = stream.mapStream( mapper );
+			s.pipe( m ).on( 'finish', function () {
+				assert.deepEqual( processed, arr );
+				done();
+			} );
+		} );
+	} );
 } );
